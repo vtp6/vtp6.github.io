28e03939a77a243206b4e07eb9f6d47d46851984
nayakrujul <nayakrujul@hotmail.com>
2023-11-01 19:38:17 +0000

    Add some spacing

--- load.js
+++ load.js
@@ -3,7 +3,11 @@
 const ll = document.getElementById("linklist");
 
 LIST_OF_LINKS.forEach(s => {
-    let newli = document.createElement("li");
-    newli.innerHTML = `<a href="./sets/` + s.replaceAll(" ", "") + `">` + s + `</a>`;
-    ll.appendChild(newli);
+    if (s === "") {
+        ll.appendChild(document.createElement("br"));
+    } else {
+        let newli = document.createElement("li");
+        newli.innerHTML = `<a href="./sets/` + s.replaceAll(" ", "") + `">` + s + `</a>`;
+        ll.appendChild(newli);
+    }
 });
