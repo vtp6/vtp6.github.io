8a81d9198fd07343a55065666748c71c11c99877
nayakrujul <nayakrujul@hotmail.com>
2023-11-03 08:16:48 +0000

    Subject folders

--- load.js
+++ load.js
@@ -1,13 +1,21 @@
 // Load the links onto the main page
 
-const ll = document.getElementById("linklist");
+const sub = document.getElementById("sub");
 
-LIST_OF_LINKS.forEach(s => {
-    if (s === "") {
-        ll.appendChild(document.createElement("br"));
-    } else {
+LIST_OF_LINKS.forEach(p => {
+    let [a, b] = p;
+    let det = document.createElement("details");
+    let sum = document.createElement("summary");
+    sum.innerHTML = a;
+    det.appendChild(sum);
+    let ul = document.createElement("ul");
+    b.forEach(s => {
         let newli = document.createElement("li");
-        newli.innerHTML = `<a href="./sets/` + s.replaceAll(" ", "") + `">` + s + `</a>`;
-        ll.appendChild(newli);
-    }
+        newli.innerHTML = `<a href="./sets/` + (a + s).replaceAll(" ", "") + `">` + s + `</a>`;
+        ul.appendChild(newli);
+    });
+    det.appendChild(ul);
+    document.body.insertBefore(det, sub);
+    document.body.insertBefore(document.createElement("br"), sub);
+    document.body.insertBefore(document.createElement("br"), sub);
 });
