9fc4f0fbe4e86f266e2927914855274caad8985c
nayakrujul <nayakrujul@hotmail.com>
2023-11-03 08:58:59 +0000

    Prettify the code

--- sets/main.js
+++ sets/main.js
@@ -1,4 +1,4 @@
-let lst = words.split("\n").map(l => l.split("\t"));
+let lst = words.split("\n").map((l) => l.split("\t"));
 
 let h1 = document.querySelector("h1");
 h1.innerHTML = `<a href="../../">VTP6</a>`;
@@ -14,245 +14,270 @@ let id = 0;
 const PAIRS = 6;
 
 function random_choice(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function random_shuffle(ls) {
-    let a = [];
-    let l = [...ls];
-    [...l].forEach(_ => {
-        let x = random_choice(l);
-        a.push(x);
-        l.splice(l.indexOf(x), 1);
-    });
-    return a;
+  let a = [];
+  let l = [...ls];
+  [...l].forEach((_) => {
+    let x = random_choice(l);
+    a.push(x);
+    l.splice(l.indexOf(x), 1);
+  });
+  return a;
 }
 
 function remove_punctuation(string) {
-    return [...string].map(c => {
-        let ord = c.charCodeAt(0)
-        return ((64 < ord && ord < 91) ||
-            (96 < ord && ord < 123)) ? c : "";
-    }).join("");
+  return [...string]
+    .map((c) => {
+      let ord = c.charCodeAt(0);
+      return (64 < ord && ord < 91) || (96 < ord && ord < 123) ? c : "";
+    })
+    .join("");
 }
 
 function check_input_classic() {
-    total++;
-    /* Easy mode: (document.getElementById("inp").value.toLowerCase().split(" ").map(q => 
+  total++;
+  /* Easy mode: (document.getElementById("inp").value.toLowerCase().split(" ").map(q => 
         answer.toLowerCase().split("/").map(j => j.split(", ")).flat()
         .map(k => k.split(" ")).flat().map(r => 
             remove_punctuation(r) === remove_punctuation(q)
         )
     ).flat().includes(true)) */
-    if (answer.toLowerCase().split("/").map(q => q.split(", ")).flat()
-        .map(r => remove_punctuation(r))
-        .includes(remove_punctuation(
-            document.getElementById("inp").value.toLowerCase()))) {
-        correct++;
-        document.getElementById("msg").innerHTML = "Correct!";
-    } else {
-        document.getElementById("msg").innerHTML = "Wrong: " + answer;
-        wrong.push([
-            document.getElementById("qs").innerHTML, 
-            answer.toLowerCase(), 
-            document.getElementById("inp").value.toLowerCase()
-        ]);
-    }
-    document.getElementById("sb").innerHTML = correct + "/" + total +
-        " (" + (correct / total * 100).toFixed(2) + "%)";
-    if (total < +document.getElementById("sld").value) {
-        new_question_classic();
-    } else {
-        document.getElementById("inp").value = "";
-        document.getElementById("inp").disabled = true;
-        document.getElementById("qs").innerHTML =
-            `<input type=button onclick="javascript:location.reload();" value="Restart" />`;
+  if (
+    answer
+      .toLowerCase()
+      .split("/")
+      .map((q) => q.split(", "))
+      .flat()
+      .map((r) => remove_punctuation(r))
+      .includes(
+        remove_punctuation(document.getElementById("inp").value.toLowerCase()),
+      )
+  ) {
+    correct++;
+    document.getElementById("msg").innerHTML = "Correct!";
+  } else {
+    document.getElementById("msg").innerHTML = "Wrong: " + answer;
+    wrong.push([
+      document.getElementById("qs").innerHTML,
+      answer.toLowerCase(),
+      document.getElementById("inp").value.toLowerCase(),
+    ]);
+  }
+  document.getElementById("sb").innerHTML =
+    correct + "/" + total + " (" + ((correct / total) * 100).toFixed(2) + "%)";
+  if (total < +document.getElementById("sld").value) {
+    new_question_classic();
+  } else {
+    document.getElementById("inp").value = "";
+    document.getElementById("inp").disabled = true;
+    document.getElementById(
+      "qs",
+    ).innerHTML = `<input type=button onclick="javascript:location.reload();" value="Restart" />`;
 
-        document.body.insertBefore(document.createElement("br"), sub);
-        document.body.insertBefore(document.createElement("br"), sub);
+    document.body.insertBefore(document.createElement("br"), sub);
+    document.body.insertBefore(document.createElement("br"), sub);
 
-        let txt = document.createElement("h3");
-        txt.innerHTML = "Mistakes:";
-        document.body.insertBefore(txt, sub);
+    let txt = document.createElement("h3");
+    txt.innerHTML = "Mistakes:";
+    document.body.insertBefore(txt, sub);
 
-        let wrongtbl = document.createElement("table");
+    let wrongtbl = document.createElement("table");
 
-        let hd = document.createElement("tr");
-        hd.innerHTML = `<th>Term</th> <th>Definition</th> <th>Your answer</th>`;
-        wrongtbl.appendChild(hd);
+    let hd = document.createElement("tr");
+    hd.innerHTML = `<th>Term</th> <th>Definition</th> <th>Your answer</th>`;
+    wrongtbl.appendChild(hd);
 
-        wrong.forEach(trp => {
-            let [u, v, w] = trp;
-            let tr = document.createElement("tr");
-            if (u.length > 18) {
-                u = u.slice(0, 15) + "...";
-            }
-            if (v.length > 18) {
-                v = v.slice(0, 15) + "...";
-            }
-            if (w.length > 18) {
-                w = w.slice(0, 15) + "...";
-            }
-            tr.innerHTML = `<td title="` + trp[0] + `">` +
-                u + `</td> <td title="` + trp[1] + `">` +
-                v + `</td> <td title="` + trp[2] + `">` +
-                w + `</td>`;
-            wrongtbl.appendChild(tr);
-        });
+    wrong.forEach((trp) => {
+      let [u, v, w] = trp;
+      let tr = document.createElement("tr");
+      if (u.length > 18) {
+        u = u.slice(0, 15) + "...";
+      }
+      if (v.length > 18) {
+        v = v.slice(0, 15) + "...";
+      }
+      if (w.length > 18) {
+        w = w.slice(0, 15) + "...";
+      }
+      tr.innerHTML =
+        `<td title="` +
+        trp[0] +
+        `">` +
+        u +
+        `</td> <td title="` +
+        trp[1] +
+        `">` +
+        v +
+        `</td> <td title="` +
+        trp[2] +
+        `">` +
+        w +
+        `</td>`;
+      wrongtbl.appendChild(tr);
+    });
 
-        document.body.insertBefore(wrongtbl, sub);
-
-    }
+    document.body.insertBefore(wrongtbl, sub);
+  }
 }
 
 function new_question_classic() {
-    document.getElementById("inp").value = "";
-    document.getElementById("inp").focus();
-    [document.getElementById("qs").innerHTML, answer] = temp = random_choice(lst);
-    lst.splice(lst.indexOf(temp), 1);
+  document.getElementById("inp").value = "";
+  document.getElementById("inp").focus();
+  [document.getElementById("qs").innerHTML, answer] = temp = random_choice(lst);
+  lst.splice(lst.indexOf(temp), 1);
 }
 
 function start_classic() {
-    document.getElementById("tbl").hidden = true;
-    document.getElementById("btn").hidden = true;
-    document.getElementById("lbl").hidden = true;
-    document.getElementById("sld").hidden = true;
-    document.getElementById("num").hidden = true;
-    document.getElementById("game").hidden = true;
-    br1.hidden = true; br2.hidden = true; sp.hidden = true;
+  document.getElementById("tbl").hidden = true;
+  document.getElementById("btn").hidden = true;
+  document.getElementById("lbl").hidden = true;
+  document.getElementById("sld").hidden = true;
+  document.getElementById("num").hidden = true;
+  document.getElementById("game").hidden = true;
+  br1.hidden = true;
+  br2.hidden = true;
+  sp.hidden = true;
 
-    let scorebar = document.createElement("b");
-    scorebar.id = "sb";
-    scorebar.innerHTML = "0/0 (0.00%)";
-    document.body.insertBefore(scorebar, sub);
+  let scorebar = document.createElement("b");
+  scorebar.id = "sb";
+  scorebar.innerHTML = "0/0 (0.00%)";
+  document.body.insertBefore(scorebar, sub);
 
-    let question = document.createElement("h3");
-    question.id = "qs";
-    document.body.insertBefore(question, sub);
+  let question = document.createElement("h3");
+  question.id = "qs";
+  document.body.insertBefore(question, sub);
 
-    let inp = document.createElement("input");
-    inp.setAttribute("type", "text");
-    inp.id = "inp";
-    document.body.insertBefore(inp, sub);
-    inp.focus();
+  let inp = document.createElement("input");
+  inp.setAttribute("type", "text");
+  inp.id = "inp";
+  document.body.insertBefore(inp, sub);
+  inp.focus();
 
-    document.body.insertBefore(document.createElement("br"), sub);
+  document.body.insertBefore(document.createElement("br"), sub);
 
-    let messagebar = document.createElement("i");
-    messagebar.id = "msg";
-    document.body.insertBefore(messagebar, sub);
+  let messagebar = document.createElement("i");
+  messagebar.id = "msg";
+  document.body.insertBefore(messagebar, sub);
 
-    new_question_classic();
+  new_question_classic();
 
-    inp.addEventListener("keyup", ({key}) => {
-        if (key === "Enter") {
-            check_input_classic();
-        }
-    });
+  inp.addEventListener("keyup", ({ key }) => {
+    if (key === "Enter") {
+      check_input_classic();
+    }
+  });
 }
 
 function start_match() {
-    document.getElementById("tbl").hidden = true;
-    document.getElementById("btn").hidden = true;
-    document.getElementById("lbl").hidden = true;
-    document.getElementById("sld").hidden = true;
-    document.getElementById("num").hidden = true;
-    document.getElementById("game").hidden = true;
-    br1.hidden = true; br2.hidden = true; sp.hidden = true;
+  document.getElementById("tbl").hidden = true;
+  document.getElementById("btn").hidden = true;
+  document.getElementById("lbl").hidden = true;
+  document.getElementById("sld").hidden = true;
+  document.getElementById("num").hidden = true;
+  document.getElementById("game").hidden = true;
+  br1.hidden = true;
+  br2.hidden = true;
+  sp.hidden = true;
 
-    let pairs = random_shuffle(random_shuffle(lst).slice(0, PAIRS));
-    let pa = random_shuffle(pairs.map(i => i[0]));
-    let pb = random_shuffle(pairs.map(j => j[1]));
+  let pairs = random_shuffle(random_shuffle(lst).slice(0, PAIRS));
+  let pa = random_shuffle(pairs.map((i) => i[0]));
+  let pb = random_shuffle(pairs.map((j) => j[1]));
 
-    let timer = document.createElement("h3");
-    timer.innerHTML = "0.0s";
-    timer.id = "timer";
-    document.body.insertBefore(timer, sub);
+  let timer = document.createElement("h3");
+  timer.innerHTML = "0.0s";
+  timer.id = "timer";
+  document.body.insertBefore(timer, sub);
 
-    let time = 0
-    id = setInterval(() => {
-        let tmr = document.getElementById("timer");
-        tmr.innerText = (++time / 10).toFixed(1) + "s";
-    }, 100)
+  let time = 0;
+  id = setInterval(() => {
+    let tmr = document.getElementById("timer");
+    tmr.innerText = (++time / 10).toFixed(1) + "s";
+  }, 100);
 
-    let matchtbl = document.createElement("table");
-    matchtbl.id = "mt";
+  let matchtbl = document.createElement("table");
+  matchtbl.id = "mt";
 
-    [...Array(PAIRS).keys()].forEach(n => {
-        let c = pa[n]; let d = pb[n];
+  [...Array(PAIRS).keys()].forEach((n) => {
+    let c = pa[n];
+    let d = pb[n];
 
-        let div = document.createElement("tr");
+    let div = document.createElement("tr");
 
-        div.innerHTML = `<td><input type="button" class="bigbutton" value="` + c +
-            `" onclick="javascript:clicked(this)" /></td> <td><input type="button" ` +
-            `class="bigbutton" value="` + d + `" onclick="javascript:clicked(this)" /></td>`;
+    div.innerHTML =
+      `<td><input type="button" class="bigbutton" value="` +
+      c +
+      `" onclick="javascript:clicked(this)" /></td> <td><input type="button" ` +
+      `class="bigbutton" value="` +
+      d +
+      `" onclick="javascript:clicked(this)" /></td>`;
 
-        matchtbl.appendChild(div);
+    matchtbl.appendChild(div);
+  });
 
-    });
-
-    document.body.insertBefore(matchtbl, sub);
-
+  document.body.insertBefore(matchtbl, sub);
 }
 
 function clicked(elem) {
-    if ([...elem.classList].includes("done")) {
-        return;
+  if ([...elem.classList].includes("done")) {
+    return;
+  }
+  if ([...elem.classList].includes("selected")) {
+    elem.classList.remove("selected");
+  } else {
+    elem.classList.add("selected");
+    let q = [...document.querySelectorAll(".selected")];
+    if (q.length >= 2) {
+      q.forEach((qq) => qq.classList.remove("selected"));
+      let [qt, qd] = q.slice(0, 2);
+      if (
+        lst.find(
+          (elm) =>
+            elm + "" === [qt.value, qd.value] + "" ||
+            elm + "" === [qd.value, qt.value] + "",
+        )
+      ) {
+        qt.classList.add("done");
+        qd.classList.add("done");
+        qt.value = "";
+        qd.value = "";
+      }
+
+      if ([...document.querySelectorAll(".done")].length >= PAIRS * 2) {
+        clearInterval(id);
+
+        let restart = document.createElement("input");
+        restart.setAttribute("type", "button");
+        restart.value = "Restart";
+        restart.onclick = () => location.reload();
+        document.body.insertBefore(restart, document.getElementById("mt"));
+      }
     }
-    if ([...elem.classList].includes("selected")) {
-        elem.classList.remove("selected");
-    } else {
-        elem.classList.add("selected");
-        let q = [...document.querySelectorAll(".selected")]
-        if (q.length >= 2) {
-            q.forEach(qq => qq.classList.remove("selected"));
-            let [qt, qd] = q.slice(0, 2);
-            if (
-                lst.find(elm =>
-                    (elm + "" === [qt.value, qd.value] + "")
-                    || (elm + "" === [qd.value, qt.value] + "")
-                )
-            ) {
-                qt.classList.add("done");
-                qd.classList.add("done");
-                qt.value = "";
-                qd.value = "";
-            }
-
-            if ([...document.querySelectorAll(".done")].length >= PAIRS * 2) {
-                clearInterval(id);
-
-                let restart = document.createElement("input");
-                restart.setAttribute("type", "button");
-                restart.value = "Restart";
-                restart.onclick = () => location.reload();
-                document.body.insertBefore(restart, document.getElementById("mt"));
-            }
-
-        }
-    }
+  }
 }
 
 function start() {
-    let selected = document.getElementById("game").value;
-    if (selected === "classic") {
-        start_classic();
-    } else if (selected === "match") {
-        start_match();
-    } else {
-        alert("Unimplemented.");
-        document.getElementById("game").value = "classic";
-    }
+  let selected = document.getElementById("game").value;
+  if (selected === "classic") {
+    start_classic();
+  } else if (selected === "match") {
+    start_match();
+  } else {
+    alert("Unimplemented.");
+    document.getElementById("game").value = "classic";
+  }
 }
 
 function update_slider() {
-    let selected = document.getElementById("game").value;
-    let sldr = document.getElementById("sld");
-    if (selected === "classic") {
-        sldr.disabled = false;
-    } else {
-        sldr.disabled = true;
-    }
+  let selected = document.getElementById("game").value;
+  let sldr = document.getElementById("sld");
+  if (selected === "classic") {
+    sldr.disabled = false;
+  } else {
+    sldr.disabled = true;
+  }
 }
 
 let slider = document.createElement("input");
@@ -273,8 +298,8 @@ document.body.insertBefore(label, slider);
 let num = document.createElement("el");
 num.innerHTML = lst.length + " &emsp;";
 slider.oninput = () => {
-    num.innerHTML = slider.value + " &emsp;";
-}
+  num.innerHTML = slider.value + " &emsp;";
+};
 num.id = "num";
 document.body.appendChild(num);
 
@@ -302,8 +327,8 @@ btn.onclick = start;
 btn.id = "btn";
 document.body.appendChild(btn);
 
-document.body.appendChild(br1 = document.createElement("br"));
-document.body.appendChild(br2 = document.createElement("br"));
+document.body.appendChild((br1 = document.createElement("br")));
+document.body.appendChild((br2 = document.createElement("br")));
 
 let table = document.createElement("table");
 table.id = "tbl";
@@ -312,19 +337,27 @@ let head = document.createElement("tr");
 head.innerHTML = `<th>Term</th> <th>Definition</th>`;
 table.appendChild(head);
 
-lst.forEach(x => {
-    let [a, b] = x;
-    let row = document.createElement("tr");
-    if (a.length > 20) {
-        a = a.slice(0, 17) + "...";
-    }
-    if (b.length > 20) {
-        b = b.slice(0, 17) + "...";
-    }
-    row.innerHTML = `<td title="` + x[0] + `">` + a +
-        `</td> <td title="` + x[1] + `">` + b + `</td>`;
-    table.appendChild(row);
-})
+lst.forEach((x) => {
+  let [a, b] = x;
+  let row = document.createElement("tr");
+  if (a.length > 20) {
+    a = a.slice(0, 17) + "...";
+  }
+  if (b.length > 20) {
+    b = b.slice(0, 17) + "...";
+  }
+  row.innerHTML =
+    `<td title="` +
+    x[0] +
+    `">` +
+    a +
+    `</td> <td title="` +
+    x[1] +
+    `">` +
+    b +
+    `</td>`;
+  table.appendChild(row);
+});
 
 document.body.appendChild(table);
 
