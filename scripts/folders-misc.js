352bb11b0ef5deb53f0b38b688425a72c5573de1
nayakrujul <nayakrujul@hotmail.com>
2024-06-19 14:47:16 +0100

    JS updates

--- scripts/folders-misc.js
+++ scripts/folders-misc.js
@@ -2,8 +2,11 @@ let units_flexboxes = [...document.querySelectorAll("div.units-flexbox")];
 
 units_flexboxes.forEach(flexbox => {
     flexbox.addEventListener("click", (event) => {
-        let checkbox = event.target.querySelector(`input[type="checkbox"]`);
+        let target = event.target;
+        if (target.nodeName === "INPUT") return;
+        while (target.nodeName !== "DIV") target = target.parentElement;
+        let checkbox = target.querySelector(`input[type="checkbox"]`);
         console.log(checkbox);
         checkbox.checked = !checkbox.checked;
-    })
+    }, true);
 })
