d2a1bf8d11868f9a80202322a45c635a18ecc832
nayakrujul <nayakrujul@hotmail.com>
2024-06-29 20:03:59 +0530

    Preparation for Classic mode implementation

--- scripts/folders-classic.js
+++ scripts/folders-classic.js
@@ -1,13 +1,26 @@
 let full_terms_list = [];
 let randomised_terms = [];
 
+function set_progress_bar_background(val, col) {
+    document.getElementById("progress-bar-container").style.background =
+        `linear-gradient(to right, ${col} ${val * 100}%, var(--other-colour) ${(1 - val) * 100}%)`;
+}
+
 function folders_start_classic(terms) {
     full_terms_list = [...terms];
     randomised_terms = random_shuffle(terms);
 
     let classic_div = document.createElement("div");
     classic_div.innerHTML = `
-        
+        <div id="progress-bar-div">
+            <div id="progress-bar-container"></div>
+            <span id="progress-bar-text">0/0 (0.00%)</span>
+        </div>
+        <h1 id="classic-question">
+            <span id="classic-question-text"></span>
+            <button class="start-button" id="finish-button">Finish!</button>
+        </h1>
+        <input type="text" id="classic-input" placeholder="Type the definition here..." />
     `;
     classic_div.id = "classic-div";
     document.getElementById("content")
