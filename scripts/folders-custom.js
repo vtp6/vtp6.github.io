d8e7e04c99b24c9fec532965b2175dfa03e46080
nayakrujul <nayakrujul@hotmail.com>
2024-10-19 18:47:41 +0100

    File formats

--- scripts/folders-custom.js
+++ scripts/folders-custom.js
@@ -1,6 +1,7 @@
 const upload = document.getElementById("input-upload");
 const file_text = document.getElementById("file-upload-text");
 const upload_go = document.getElementById("upload-go-button");
+const upload_format = document.getElementById("file-format");
 
 let terms = [];
 
@@ -8,8 +9,57 @@ function read_vtp6_format(text) {
     let lines = text.split("\n");
     lines.forEach(line => {
         let spl = line.split("\t");
-        if (spl.length == 2) {
+        if (spl.length === 2) {
             terms.push(spl);
+        } else {
+            console.error("Invalid line: " + line);
+        }
+    });
+}
+
+function read_vtp5_format(text) {
+    let lines = text.split("\n");
+    for (let i = 0; i < lines.length; i += 2) {
+        terms.push([lines[i], lines[i + 1]]);
+    }
+}
+
+function read_csv_format(text) {
+    text.split("\n").forEach(line => {
+        let result = [];
+        let curr = "";
+        let quotes = "";
+        let escaped = false;
+        for (let ind = 0; ind < line.length; ind++) {
+            let char = line[ind];
+            if (escaped) {
+                curr += char;
+                escaped = false;
+                continue;
+            }
+            if (char === "," && quotes === "") {
+                result.push(curr);
+                curr = "";
+                continue;
+            }
+            if (char === "'" || '"' === char) {
+                if (quotes === char && !escaped) {
+                    quotes = "";
+                } else {
+                    quotes = char;
+                }
+                continue;
+            }
+            if (char === "\\") {
+                escaped = true;
+                continue;
+            }
+            curr += char;
+        }
+        result.push(curr);
+        return result;
+        if (result.length === 2) {
+            terms.push(result);
         } else {
             console.error("Invalid line: " + line);
         }
@@ -18,10 +68,10 @@ function read_vtp6_format(text) {
 
 upload.addEventListener("input", () => {
     let n = upload.files.length
-    if (n == 0) {
+    if (n === 0) {
         file_text.innerHTML = `<i>No files selected</i>`;
         upload_go.disabled = true;
-    } else if (n == 1) {
+    } else if (n === 1) {
         file_text.innerHTML = `<i>1 file selected</i>`;
         upload_go.disabled = false;
     } else {
@@ -36,7 +86,15 @@ upload_go.addEventListener("click", () => {
         if (file) {
             let reader = new FileReader();
             reader.addEventListener("load", () => {
-                read_vtp6_format(reader.result)
+                let splits = file.name.split(".");
+                if (splits[splits.length - 1].toLowerCase() == "csv" || upload_format.value === "csv") {
+                    read_csv_format(reader.result);
+                }
+                if (upload_format.value === "vtp6") {
+                    read_vtp6_format(reader.result);
+                } else if (upload_format.value === "vtp5") {
+                    read_vtp5_format(reader.result);
+                }
             }, false);
             reader.readAsText(file);
         }
