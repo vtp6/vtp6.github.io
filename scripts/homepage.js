939534e0c07118cb65e3ba677b7e79a0d681c1da
nayakrujul <nayakrujul@hotmail.com>
2025-02-09 14:31:03 +0000

    Homepage updates

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,8 @@
 const orange_text = document.getElementById("orange");
 const purple_text = document.getElementById("purple");
+const heading_h1 = document.getElementById("main-header");
+const big_logo = document.getElementById("big-logo");
+const logo_text = document.getElementById("big-logo-text");
 
 let animation_text = [..."Vocabulary Testing Program"];
 
@@ -17,7 +20,20 @@ let n = 1;
 
 function increment() {
     purple_text.innerHTML = "" + n;
-    if (++n <= 6) setTimeout(increment, 50)
+    if (++n <= 6) setTimeout(increment, 100)
 }
 
-next_run();
+function animation() {
+    setTimeout(() => {
+        big_logo.classList.add("move-right");
+        logo_text.classList.add("show");
+    }, 500);
+    setTimeout(() => {
+        heading_h1.classList.remove("hidden");
+        big_logo.classList.add("hidden");
+        logo_text.classList.remove("show");
+        increment();
+    }, 2000);
+}
+
+animation();
