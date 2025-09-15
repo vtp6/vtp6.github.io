7e1e7ecf75a53d44d17c611cb410e6155971e2e4
nayakrujul <nayakrujul@hotmail.com>
2025-09-15 11:46:30 +0100

    Remove local storage port

--- scripts/lightdark.js
+++ scripts/lightdark.js
@@ -1,27 +1,3 @@
-function get_cookies() {
-    let str = document.cookie.split(";");
-    let d = {};
-    for (i = 0; i < str.length; i++) {
-        if (str[i] === "") continue;
-        let c = str[i].split("=");
-        if (d[c[0].trim()] === undefined)
-            d[c[0].trim()] = c[1].trim();
-    }
-    return d;
-}
-
-
-// Port to local storage
-
-if (localStorage.length === 0) {
-    d = get_cookies();
-    for (key in d) {
-        if (key !== "cookiesAllowed" && key !== "vtp6NewLogo1")
-            localStorage.setItem(key, d[key]);
-    }
-}
-
-
 function switch_theme() {
     document.body.style.animation = "";
     document.body.offsetWidth;
